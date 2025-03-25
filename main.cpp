4f80e96df5f5f87fd02c3073fdfdb7701b1dfaa9
wangfra27 <wangfra@umich.edu>
2025-03-25 04:32:45 -0400

    updated with overwrite

--- main.cpp
+++ main.cpp
@@ -1,17 +1,59 @@
 #include <iostream>
 #include "ring_buffer.h"
-#include "uart.h"
+
+using namespace std;
+
+void startBuffer(RingBuffer<char, 5>& buffer) {
+    const char* data = "ABCDE";
+    for (int i = 0; data[i] != '\0'; i++) {
+        buffer.push(data[i]);
+        cout << "Added: " << data[i] << endl;
+    }
+}
+
+void addData(RingBuffer<char, 5>& buffer, char c) {
+    buffer.push(c);
+    cout << "Added: " << c << endl;
+}
+
+void removeData(RingBuffer<char, 5>& buffer) {
+    char c;
+    buffer.pop(c);
+    cout << "Received: " << c << endl;
+}
 
 int main() {
-    RingBuffer rb(5);
-    UART uart(9600);
+    RingBuffer<char, 5> buffer;
 
-    rb.push(10);
-    rb.push(20);
-    std::cout << "Popped: " << rb.pop() << std::endl;
+    cout << "Start buffer" << endl;
+    startBuffer(buffer);
 
-    uart.sendData("Hello, UART!");
-    std::cout << "Received: " << uart.receiveData() << std::endl;
+    cout << "Process buffer" << endl;
+    removeData(buffer);
+    removeData(buffer);
+    removeData(buffer);
+
+    cout << "Overwrite buffer" << endl;
+    buffer.push('F');
+    buffer.push('G');
+    
+    cout << "Process buffer" << endl;
+    removeData(buffer);
+    removeData(buffer);
+
+    buffer.push('H');
+    buffer.push('I');
+    
+    cout << "Process buffer" << endl;
+    removeData(buffer);
+    removeData(buffer);
+
+    buffer.push('J');
+
+    cout << "Process buffer" << endl;
+    removeData(buffer);
+    removeData(buffer);
 
     return 0;
 }
+
--- ring_buffer.h
+++ ring_buffer.h
@@ -1,21 +1,42 @@
-#ifndef RING_BUFFER_H
-#define RING_BUFFER_H
+#include <iostream>
+#include <array>
+#include <atomic>
 
-#include <vector>
-
+template <typename T, size_t Size>
 class RingBuffer {
-private:
-    std::vector<int> buffer;
-    int head, tail, size, capacity;
-
 public:
-    RingBuffer(int cap);
-    ~RingBuffer();
+    RingBuffer() : head(0), tail(0), count(0) {}
 
-    void push(int value);
-    int pop();
-    bool isEmpty() const;
-    bool isFull() const;
+    void push(T value) {
+        if (count == Size) {
+            tail = (tail + 1) % Size;
+        } else {
+            count++;
+        }
+        buffer[head] = value;
+        head = (head + 1) % Size;
+    }
+
+    bool pop(T& value) {
+        if (count == 0) {
+            return false;
+        }
+        value = buffer[tail];
+        tail = (tail + 1) % Size;
+        count--;
+        return true;
+    }
+
+    bool isEmpty() const {
+        return count == 0;
+    }
+    
+    bool isFull() const {
+        return count == Size;
+    }
+
+private:
+    std::array<T, Size> buffer;
+    size_t head, tail;
+    std::atomic<size_t> count;
 };
-
-#endif
